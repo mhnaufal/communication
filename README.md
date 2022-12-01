e5527c354e063ae0279205af1670520101596195
mhnaufal <aal.pirates16@gmail.com>
2022-12-01 08:33:03 +0700

    DONE the cherno thread

---
+++ README.md
@@ -0,0 +1,16 @@
+# C++ Threading
+
+## Thread
+
+1. **Program** = a running code + data
+2. **Process** = a running program, one program can have multiple processes
+3. **Thread** = a unit of execution inside a process, one process can have multiple threads
+4. **Join** = the same as wait, block current thread until this other thread had completed
+
+## IPC/RPC
+
+## Credits
+
+1. https://www.youtube.com/results?search_query=c%2B%2B+thread
+2. https://www.educba.com/c-plus-plus-thread/
+3. https://en.cppreference.com/w/cpp/thread/thread
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+static bool is_finish = false;
+
+void printLog()
+{
+  using namespace std::literals::chrono_literals;
+
+  while (!is_finish) {
+    auto start_time = std::chrono::system_clock::now();
+    std::this_thread::sleep_for(1s);
+    auto end_time = std::chrono::system_clock::now();
+    std::chrono::duration<float> duration = end_time - start_time;
+
+    std::cout << "[INFO] running on thread " << std::this_thread::get_id()
+              << " on sec: " << duration.count() << std::endl;
+  }
+
+  std::cout << "[DEBUG] thread " << std::this_thread::get_id() << " finished"
+            << std::endl;
+}
+
+int main()
+{
+  std::thread worker_1(printLog);
+  std::cout << worker_1.get_id() << std::endl;
+
+  std::cin.get();
+  is_finish = true;
+
+  worker_1.join();  // force the "main thread" to wait until the worker_1
+                    // thread finished
+
+  return 0;
+}
