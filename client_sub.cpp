52ce36ba00fbb911ae60319a14ed4173492d7127
mhnaufal <aal.pirates16@gmail.com>
2022-12-06 09:51:02 +0700

    FAILED: c++ python

---
+++ client_sub.cpp
@@ -0,0 +1,20 @@
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <msgpack.hpp>
+#include <mutex>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/all_layers.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <thread>
+#include <zmq.hpp>
+#include <zmq_addon.hpp>
+
+int main()
+{
+
+
+  return EXIT_SUCCESS;
+}
