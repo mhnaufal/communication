9a980aa54fb3ff2db9cf5b2e4ef261f66c1ec6de
mhnaufal <aal.pirates16@gmail.com>
2022-12-07 14:34:00 +0700

    checkpoint: cv::Mat unpacked data

--- cv_msg.cpp
+++ cv_msg.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+  /* prepare data */
   std::string tag = "tag";
   const std::time_t now = std::time(nullptr);
   std::string text = "pack_this!";
@@ -21,11 +22,34 @@ int main()
             << img.empty() << std::endl;
   //  for (auto item : m1.img) std::cout << item << std::endl;
 
+  /* packing data */
   msgpack::sbuffer packed_data;
   msgpack::pack(&packed_data, m1);
 
   std::cout << "[PACKED] " << packed_data.data() << " || " << packed_data.size()
             << std::endl;
+
+  /* unpacking data */
+  msgpack::unpacked unpacked_data;
+  msgpack::unpack(unpacked_data, static_cast<const char*>(packed_data.data()),
+                  packed_data.size());
+
+  auto returned_data = unpacked_data.get().convert(m1);
+  std::cout << "[UNPACKED] " << unpacked_data.get() << " || "
+            << returned_data.tag << " || " << returned_data.time << " || "
+            << returned_data.text << returned_data.img.empty() << std::endl;
+
+  //  auto ss = cv::Mat(2, 2, CV_8UC3, cv::Scalar(126, 0, 255));
+  //  auto new_img = std::vector<uchar>(
+  //      ss.data, ss.data + (ss.rows * ss.cols * ss.channels()));
+
+  auto new_img = returned_data.img;
+  bool is_equal = cv::countNonZero(new_img != m1.img) == 0;
+
+  if (is_equal)
+    std::cout << "IT'S THE SAME!" << std::endl;
+  else
+    std::cout << "FUCKING DIFFERENT!!!" << std::endl;
 
   return EXIT_SUCCESS;
 }
